a85b4df6a0ff417c507541bf52a18fe3437a6cde
priyanka Biradar <pbiradar397@gmail.com>
2022-06-01 16:54:53 +0530

    printing sum using for

---
+++ src/PrintNumbersFor.java
@@ -0,0 +1,11 @@
+public class PrintNumbersFor {
+
+    public void add() {
+
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
+    }
+}
---
+++ src/PrintReverse.java
@@ -0,0 +1,8 @@
+public class PrintReverse {
+    public void reverse() {
+
+        for(int i=100;i>=0;i--) {
+            System.out.println(i);
+        }
+    }
+}
