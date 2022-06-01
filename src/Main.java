e2e5efd4fb5db53e3379a1ea8af70d6651082ea7
priyanka Biradar <pbiradar397@gmail.com>
2022-06-01 14:21:37 +0530

    printing name using for

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         Increment i = new Increment();
-        LoopSwitch mon= new LoopSwitch();
+        LoopSwitch mon = new LoopSwitch();
 
 //        i.playWithOperators();
         //   i.condition();
 //        i.monthsInaYear();
         mon.loop();
+        NameForLoop n = new NameForLoop();
+        n.name();
 
 
     }
---
+++ src/NameForLoop.java
@@ -0,0 +1,12 @@
+public class NameForLoop {
+    public void name() {
+
+//    int i;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Priyanka");
+
+        }
+    }
+
+}
+
