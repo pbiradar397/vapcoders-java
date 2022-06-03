52d1eab67000f7995d761a08c09671b0afc50887
priyanka Biradar <pbiradar397@gmail.com>
2022-06-03 21:52:58 +0530

    correction of ReversePattern.java

--- src/ReversePattern.java
+++ src/ReversePattern.java
@@ -1,7 +1,7 @@
 public class ReversePattern {
     public void reverse() {
-        for (int i = 1; i < 5; i++) {
-            for (int j = 4; j>=i; j--) {
+        for (int i = 4; i >=1; i--) {
+            for (int j = 1; j<=i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
